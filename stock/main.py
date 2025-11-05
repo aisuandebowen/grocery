bc000b8f58956c87bc7a5b3012c62d6ec638803d
Adam <boenchen0839@gmail.com>
2025-11-05 18:06:03 +0800

    feat:股票基本信息读取

--- stock/main.py
+++ stock/main.py
@@ -1,4 +1,5 @@
 import asyncio  # 导入asyncio模块
+import re
 
 from http_server import get_client
 
@@ -14,8 +15,7 @@ async def get_exchange_rate():
     }
     path = 'v4/latest/CNY'
     exchange_api_client = get_client(base_url=BASE_URLS["exchangerate-api"])
-    response = await exchange_api_client.get(endpoint=path)
-    print(response)
+    return await exchange_api_client.get(endpoint=path)
 
 
 async def get_stock_info():
@@ -24,9 +24,17 @@ async def get_stock_info():
         "tencent_api": "https://qt.gtimg.cn"
     }
     stock_client = get_client(base_url=BASE_URLS["tencent_api"])
-    params = {'q': 'sz000858'}
+    stocks_goal = {'sh601127': {'b': 130.00}, 'sh601021': {'b': 52.00}, 'sz002583': {'b': 11.00},
+                   'sz002765': {'b': 12.50}}
+    params = {'q': ','.join(stocks_goal.keys())}
     response = await stock_client.get(params=params)
-    print(response)
+    box = response['data'].split(';')[0]
+    match = re.search(r'"(.*?)"', box)
+    if match:
+        stock_info = match.group(1).split('~')
+        code = stock_info[2]
+        price = stock_info[3]
+        print(code, price)
 
 
 def job():
@@ -34,8 +42,16 @@ def job():
 
 
 async def main():
-    # await get_stock_info()
-    await get_exchange_rate()
+    await get_stock_info()
+    # response = await get_exchange_rate()
+    # exchange_rates = response['data']['rates']
+    # to_JPY = exchange_rates['JPY']
+    # to_USD = exchange_rates['USD']
+    # to_EUR = exchange_rates['EUR']
+    # exchange_goal = {'JPY': 22.00, 'USD': 7.00, 'EUR': 7.50}
+    # RMB_to_JPY = to_JPY
+    # USD_to_RMB = 1 / to_USD
+    # EUR_to_RMB = 1 / to_EUR
 
 
 if __name__ == "__main__":
